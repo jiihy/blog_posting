c1677e778cae7cb9d5004dbdaffd4083d6ccd5f2
rosie <92841833+jiihy@users.noreply.github.com>
2022-09-25 16:49:40 +0900

    Create 2022-07-06-shamagotchi.m

---
+++ _posts/co-working/2022-07-06-shamagotchi.m
@@ -0,0 +1,66 @@
+---
+title: 샤마고치 프로젝트 (Shamagotchi)
+categories: [co-working]
+comments: true
+---
+
+```
+[ 기간 ] 2개월 (4~5월)
+
+[ 팀원 ] 3명 
+
+[ 성과 ] 뉴욕 진출 ...?!
+```
+
+https://postfiles.pstatic.net/MjAyMjA3MDVfMjk3/MDAxNjU3MDE0NDE4NDI3.agjpWrp3io406p0qo2zrB6yCU6Qad-hbuzOHSZlAZBEg.J80X8fHwW62ct9PbjeGeloaCruY7LWvyzQKky6HtnQcg.JPEG.jiihy/SE-c0c4e8c1-c2b4-4e58-9bdc-a5b8916b5977.jpg?type=w966
+`뉴욕 Soul Night 행사에 전시된 샤마고치 (쩌렁쩌렁)`
+
+<br>
+민팅 후, 누가 돈 주는 것도 아닌데 주체적으로 본인의 고스트를 활용하여 갖고 노는 커뮤니티 분위기 때문이었는지 나도 눈에 띄는 프로젝트를 기획하고 싶은 생각이 들었다. J언니와 NFT 이야기를 주고받으며 자주 티타임을 가지던 시기다. 
+<br><br>
+초반 샤고스 홀더 베네핏에서 느낀 키워드는 "다양한 사람들과 재밌게 주고받는 커리어 영감의 기회"였다. 크게는 브루클린 프로젝트, 소소하게는 연사들의 온라인 강연이 그랬다. NFT 구매로 web3세계에 입문하고 직업적 확장까지 가져갈 수 있다면 내가 투자한 5마리의 고스트가 아깝지 않을 것이다. 그렇게 우리 프로젝트의 가닥은 "즐기면서 성장하는 커리어 스터디"가 됐다. 이왕 하는 거 팀원 모두가 각자의 포트폴리오로 써먹을 수 있는 작업이 됐으면 좋겠다는 게 가장 큰 바람이었다. 샤고스도 홀더들이 그렇게 성장하기를 원한다고 생각했다.
+<br><br>
+다시 티타임으로 돌아가서, 뭘 만들면 좋을까? 제각기 다른 모습의 귀여운 고스트... 너무 귀여워서 밥도 주고 재워주고 다 해주고 싶다...!아! 웹사이트로 다마고치를 구현해 볼까?! 가 샤마고치의 탄생이다.(나와 J언니는 html웹사이트 만드는 일을 한다.) 짤막하게 먹었던 외국물을 좋아하는 우리는 프로젝트의 해외 진출까지 꿈꾸고 있었다. 한때 해외에서 붐이 일어났던 다마고치는 여러모로 최고의 소스인 셈이다. (ㅋㅋ)
+<br><br>
+
+> 디자인
+https://postfiles.pstatic.net/MjAyMjA3MDVfMTU2/MDAxNjU3MDE2NDUyOTU3.tp4Hkyg2C956Cm1VUnkJnPqC6aN2i8KE1Er4D4is-nog.1E1em2p4b-sQV4EzV2GJRBPTssTdgzQpV1k-aaxhCnsg.PNG.jiihy/0867_(1).png?type=w966
+https://postfiles.pstatic.net/MjAyMjA3MDVfMjI1/MDAxNjU3MDE2NDUzMjQw.vVKKe8UhsHS5X7lBxajn1jNJzse4fwT4ikuOtHHAdNsg.hxAUvlhV-gITzJxtAwVyTOgy_yVbaW0rZhkXEjF7Rg4g.PNG.jiihy/0868.png?type=w966
+
+우리는 기존 샤고스 이미지를 한 번 더 변형해 귀엽게 픽셀화시켰다. 이 과정은 샤마고치 팀 디자이너 h가 맡아주었다. h는 처음에는 팀원도 홀더도 아니었으나, 나와 J언니가 크기를 줄였다 키워서 만든 픽셀괴물을 보고 '아 이건 좀..' 싶었는지 몇 번 작업을 도와줬다. 그러다 갑자기 5마리 고스트의 임보인이 되더니 샤마고치 멤버가 되었다. h에게 작업을 맡길 땐 누끼본도 필요 없이 고유번호만 있으면 된다. 픽셀을 직접 찍어 그려내기 때문이다. 우리는 첫 결과물을 본 순간부터 100% 믿고 맡길 수 있었다. 아래는 그가 그린 귀여운 작업물들
+
+<br>
+
+> 개발
+앞서 밝혔듯 우리는 같은 회사에 다니며 인터넷 웹사이트를 구현하는 일을 한다. J언니는 우리 회사의 베테랑 에이스다. (팀장님이 이렇게 말하심) 샤마고치의 개발과 저장소 관리는 베테랑 에이스 J언니가 담당했다. 커리어 스터디 목적의 프로젝트인 만큼 회사에서 써보지 않았던, 그러나 요즘 업계에서 너무나 중요한 'React'라는 프레임워크로 만들었다. J언니에게 "이러이러한 움직임을 넣으면 귀여울 것 같아"라고 하면 뚝딱뚝딱 구현해서 짠하고 보여준다. 나는 코드를 엉금엉금 따라가며 테스트하고 스타일 수정등을 했다. 버튼을 누르면 좌우로 이동하고 점프하며 말까지 하는 기능은 구글링으로 학습해 넣었다. 핀을 뽑아야 시작하는 화면전환부터 걸어갈 때 배경 이동, 점프 시 그림자 디테일과 하트표까지 부릴 수 있는 재주는 최선을 다해 부렸다고 해야 할까? 팀원들과 토론하며 넣기도, 빼기도 한 이 작업에 애정이 담기지 않을 수가 없다. 
+
+https://postfiles.pstatic.net/MjAyMjA3MDZfNDgg/MDAxNjU3MDk1NDI4MTg2.vUNnoxBFOOvzyGzhr_Zww2xgOEiTmVG4a_r-hmHBZTcg.LzlZo1jsfDgzIXExlhWCUB-kKSO_fo-zcj7XQ2hJdVIg.PNG.jiihy/image.png?type=w966
+`우리 저장소!`
+
+우리는 멈춰있는 사진뿐인 NFT에 살아있는 생명력을 부여했다. 꼬물꼬물 움직이는 나만의 고스트라니 얼마나 귀여운가! 이를 휴대폰이나 스마트워치 배경화면으로 설정하니 우리만의 PFP NFT(profile picture nft)로도 사용할 수 있었다. 브루클린 프로젝트 때 만난 신혜님(샤고스 캡틴) 또한 샤마고치를 애플워치 배경화면으로 설정했다고 하셨다👍
+https://postfiles.pstatic.net/MjAyMjA3MDVfNCAg/MDAxNjU3MDE1MzUzMTA0.BoPMTVBuHyAZ5aMwgKS3vGisM5iCGUw1G2tt5sZLYXUg.azuBqg87t_mgZqUnloT8tus6tkaBsCmRtyIH0Ejee2Eg.JPEG.jiihy/IMG_5097.jpg?type=w966
+https://postfiles.pstatic.net/MjAyMjA3MDVfMTY0/MDAxNjU3MDE1MzUzMTY3.8AbeDqP7OUyQVkzCoIlwkCdO1kmO5p-TGpMJNwbHje0g.vSlxUQvOmZgy8Dy267KSZxY4IIXGun7zQ6kO6_ZG7a8g.JPEG.jiihy/IMG_5096.jpg?type=w966
+https://postfiles.pstatic.net/MjAyMjA3MDVfNjkg/MDAxNjU3MDE1MzUzMjM3.DjrAJiVcaQMZmN8he_t2uz6ErcfNdOqWjV7N6kqQzkMg.wjvF8tc2vKAfbUCR7MpQpFtAczTFAOfvYGrEoRMRTxsg.JPEG.jiihy/IMG_5095.jpg?type=w966
+
+> 샤마고치 영향력
+
+샤마고치 공식 홈페이지와 등록되지 않은 번호는 물음표 고스트가 뜨도록 설정했고, 신청받은 고스트는 커스텀 하여 홈페이지 고유 링크로 등록했다. 약 일주일 동안 23명의 홀더분들이 구매하여 총 208KLAY의 수익을 얻었다. (당시 1klay=800원)
+
+https://postfiles.pstatic.net/MjAyMjA3MDZfOTgg/MDAxNjU3MDk3MjYxNTYy.zncU0Y7P-e36Cdu8p8g3jB6uiEfmb-C2edcyskhOtS0g.Os8hh7muA-Rho3WUODE6toz5HfR-yLTArk8QclyK4_cg.JPEG.jiihy/IMG_F3A664F437EB-1.jpeg?type=w966
+우리의 프로젝트는 '오설록 티하우스 아모레 성수점'에서 많은 사람에게 노출되기도 하고,
+https://postfiles.pstatic.net/MjAyMjA3MDZfMjM3/MDAxNjU3MDk3MTMyODMy.vOlvfV2WoH7pOTWZdc6GepPd6hX2fQxutJeSwbtOfEog.aC2wF8YcKQ4SBPwS_0RaASXn9cTGNrJmAHssUnZV4ZYg.JPEG.jiihy/IMG_2454.jpg?type=w966
+
+고객참여 커뮤니티 성공 사례로 Enterprise 블록체인 ChainZ 방송에 진출했으며,
+https://postfiles.pstatic.net/MjAyMjA3MDZfODUg/MDAxNjU3MTAwODI3MTkz.LWenz3xPZSbkufQWlmvbu0T_p-g5fWM4XVRONYeqj9Ug.pPE3wBvIW-r7LcZP7Bll5rbE98Cn7mYqCwKH_oayKz8g.JPEG.jiihy/IMG_5108.jpg?type=w966
+브루클린 상공회의소 회의실에서 상공회의소장 Randy에게 직접 소개하는 영광과,
+https://postfiles.pstatic.net/MjAyMjA3MDZfODMg/MDAxNjU3MDk5ODc4NDM0.mEwO1PncM1qH9XTuV3d1lTQB3pWIsayLy06nFBPw8Rkg.be7p7aBGGZf3h1H99f7Gy6XguhClIb1nnVPZh4ppoKwg.JPEG.jiihy/SE-2dd6dee6-9321-4a2a-8327-b277f16afbcd.jpg?type=w966
+미국 뉴욕 브루클린에서 열린 'Soul Night'이라는 NFT 행사에 전시되어 세계인의 귀여움을 받기도 했다. 😋
+https://postfiles.pstatic.net/MjAyMjA3MDZfMTU3/MDAxNjU3MDk3OTU3MDI1.OeUWma5VIIv6-myfbxAZJn56R9zhqHY-xexpWT1ObA0g.XA_66UObhskdJqZVrjTHv7HSMe8-RFxopvyqiiLX-OAg.JPEG.jiihy/IMG_3555.jpg?type=w966
+
+Soul night 주최사의 개발자님과 앞으로의 샤마고치에 대해 이야기를 나누는 값진 시간도 가졌다. 
+https://postfiles.pstatic.net/MjAyMjA3MDZfODAg/MDAxNjU3MDk4MjQ3NjI0.MQP905gJ0hG-yvuWlEx6xS23e4R5Rnm86XMuFaePfhkg.-pJoJ4fAT7VJep3RwjI8XEMUw9wRQ64ngwyGpr5f6Ucg.JPEG.jiihy/IMG_3804.jpg?type=w966
+
+앞으로 샤마고치는 어떻게 변화할까?
+
+----
+회사 휴게실 티타임에서 시작된 상상이 멀리 뉴욕으로까지 퍼지는 결과물로 돌아오리라고는 생각도 못 했다. 2-3개월간의 작은 토이 프로젝트였지만 계획보다 더 큰 미래가 열린다는 게 너무 흥미롭고 재밌다! 우리 팀원들은 나에게 대표로서 프로젝트를 이끌어줘서 고맙다고 한다. 내가 한 건 판 벌리기와 우리 일하자고 티 안 나게 채찍질한 것뿐(근데 티 남) 능력자 J언니랑, h가 같이 해서 이 모든 게 가능했고 정말 많이 배웠다. 나야말로 함께해 줘서 너무너무 고마워 🤍 샤마고치 첫번째 페이지 끝 ~!
